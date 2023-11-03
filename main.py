b6866c0eff2458add6927e1b3f75c26d943fb6e9
Kiritoro <trungle30902@gmail.com>
2023-11-04 00:22:52 +0700

    Init project

---
+++ main.py
@@ -0,0 +1,36 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI( 
+  # openapi_url='/docs',
+  title='UNI Campus - API', 
+  version='0.3.1'
+)
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allow_headers=["*"]
+)
+
+@app.get("/", tags=['Root'])
+async def root(
+) -> dict:
+  """Test Endpoint"""
+  return { "message": "UNI Campus - API" }
+
+if __name__ == "__main__":
+  uvicorn.run(
+    "main:app",
+    host=os.environ.get('DOMAIN'),
+    port=int(os.environ.get('PORT')),
+    log_level="info",
+    reload=True
+  )
